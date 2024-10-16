bec4f39781047aa6be8e346cfd4024cb052dfb62
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2024-10-16 20:44:47 +0300

    add user validator with express-validator

---
+++ routes/auth_routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const { body } = require("express-validator");
+const authController = require("../controller/auth_controller");
+
+const userValidator = [
+  body("name").not().isEmpty().withMessage("Name is Required!"),
+  body("email").isEmail().withMessage("Please enter a valid email Address!"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters")
+    .isStrongPassword()
+    .withMessage(
+    "Password must contain at least one uppercase, on lowercase . and one symbol."
+    )
+    .body("phone")
+    .isMobilePhone()
+    .withMessage("Please enter a valid phone number"),
+];
+
+router.post("/login", authController.login);
+router.post("/register", userValidator, authController.register);
+router.post("/forgot-password", authController.forgetPassword);
+router.post("/verify-otp", authController.verifyPasswordResetOtp);
+router.post("/reset-password", authController.resetPassword);
+module.exports = router;
