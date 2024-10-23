3dab6ecd1dfa6d64e173d0504de5923c691a3eb6
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2024-10-23 20:46:11 +0300

    added verifyPasswordResetOtp Controller

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -75,6 +75,7 @@ exports.login = async function (req, res) {
       .status(200)
       .json({ ...user.doc, accessToken, refreshTokenToken });
   } catch (error) {
+    console.error(error);
     return res.status(500).json({
       type: error.name,
       message: error.message,
@@ -104,6 +105,7 @@ exports.forgetPassword = async function (req, res) {
     );
     return res.json({ message: response});
   } catch (error) {
+    console.error(error);
     return res.status(500).json({
       type: error.name,
       message: error.message,
@@ -134,6 +136,7 @@ exports.verifyToken = async function (req, res) {
 
     return res.json(true);
   } catch (error) {
+    console.error(error);
     return res.status(500).json({
       type: error.name,
       message: error.message,
@@ -141,6 +144,30 @@ exports.verifyToken = async function (req, res) {
   }
 };
 
-exports.verifyPasswordResetOtp = async function (req, res) {};
+exports.verifyPasswordResetOtp = async function (req, res) {
+  try {
+    const {email,otp} = req.body;
+
+    const user = await User.findOne({email});
+    if (!user) return res.status(404).json({message:"User with that email does NOT exists!"});
+
+    if(user.resetPasswordOtp !== +otp || Date.now() > user.resetPasswordOtpExpires ){
+      return res.status(401).json({message:"Invalid or expired OTP"});
+    }
+
+    user.resetPasswordOtp = 1;
+    user.resetPasswordOtpExpires = undefined;
+    await user.save();
+    return res.status(200).json({message:"Password Reset OTP verified"});
+
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      type: error.name,
+      message: error.message,
+    });
+  }
+};
 
 exports.resetPassword = async function (req, res) {};
