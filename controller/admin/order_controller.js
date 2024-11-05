8ef4184a94b3dd5280c14abd16c3a7b298d996de
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2024-11-05 21:18:57 +0300

    --added Get Order Controller

--- controller/admin/order_controller.js
+++ controller/admin/order_controller.js
@@ -1,5 +1,33 @@
-const { Order } = require('../../models/order');
-exports.getOrders = async (req, res) => {};
+const { Order } = require("../../models/order");
+exports.getOrders = async (req, res) => {
+  try {
+    const orders = await Order.find().
+    select(" -statusHistory").
+    populate('user',"name,email")
+    .sort({ dateOrdered: -1 }).populate(
+     {
+       path: "orderItems",
+       populate: {
+         path: "product",
+         select: "name",
+         populate: {
+           path: "category",
+           select: "name",
+         },
+       },
+     }
+    );
+    if (!orders) return res.status(404).json({ message: "No orders found!" });
+
+    return res.status(200).json({ orders }); 
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      type: error.name,
+      message: error.message,
+    });
+  }
+};
 exports.getOrdersCount = async (req, res) => {};
 
 exports.changeOrdersStatus = async (req, res) => {};
