35b176f10f206e70e171287229f755bf9a789c71
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2024-11-06 18:13:33 +0300

    -- added get orders count route

--- controller/admin/order_controller.js
+++ controller/admin/order_controller.js
@@ -1,25 +1,24 @@
 const { Order } = require("../../models/order");
 exports.getOrders = async (req, res) => {
   try {
-    const orders = await Order.find().
-    select(" -statusHistory").
-    populate('user',"name,email")
-    .sort({ dateOrdered: -1 }).populate(
-     {
-       path: "orderItems",
-       populate: {
-         path: "product",
-         select: "name",
-         populate: {
-           path: "category",
-           select: "name",
-         },
-       },
-     }
-    );
+    const orders = await Order.find()
+      .select(" -statusHistory")
+      .populate("user", "name,email")
+      .sort({ dateOrdered: -1 })
+      .populate({
+        path: "orderItems",
+        populate: {
+          path: "product",
+          select: "name",
+          populate: {
+            path: "category",
+            select: "name",
+          },
+        },
+      });
     if (!orders) return res.status(404).json({ message: "No orders found!" });
 
-    return res.status(200).json({ orders }); 
+    return res.status(200).json({ orders });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
@@ -28,6 +27,29 @@ exports.getOrders = async (req, res) => {
     });
   }
 };
-exports.getOrdersCount = async (req, res) => {};
+exports.getOrdersCount = async (_, res) => {
+  try {
+    const count = awaitOrder.countDocuments();
+    if (!count)
+      return res.status(500).json({ message: "Could not get the order count" });
+    return res.status(200).json({ count });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      type: error.name,
+      message: error.message,
+    });
+  }
+};
 
-exports.changeOrdersStatus = async (req, res) => {};
+exports.changeOrdersStatus = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      type: error.name,
+      message: error.message,
+    });
+  }
+};
